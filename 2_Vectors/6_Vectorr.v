40378014fe4cce526e410b54ee0e1368a2ed3b17
drgn88 <135236975+drgn88@users.noreply.github.com>
2025-04-23 18:32:42 +0900

    Add files via upload

---
+++ 2_Vectors/6_Vectorr.v
@@ -0,0 +1,24 @@
+module top_module( 
+    input [7:0] in,
+    output [7:0] out
+);
+
+    assign out[7] = in[0];
+    assign out[6] = in[1];
+    assign out[5] = in[2];
+    assign out[4] = in[3];
+    assign out[3] = in[4];
+    assign out[2] = in[5];
+    assign out[1] = in[6];
+    assign out[0] = in[7];
+endmodule
+
+`if 0
+module top_module( 
+    input [7:0] in,
+    output [7:0] out
+);
+
+    assign out = {in[0], in[1], in[2], in[3], in[4], in[5], in[6], in[7]}
+endmodule
+`endif
